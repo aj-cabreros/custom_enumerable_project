22bdb75a413e1362acb026b35917d09529a4c4fd
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-10-15 15:17:49 -1000

    [+] Complete custom enumerable definitions
    Simply created methods that pass the provided rspec test.s

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.10'
+
+gem "rubocop", "~> 1.67"
+
+gem "rubocop-performance", "~> 1.22"
--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,74 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    for item in self
+      yield(item, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    new_array = []
+    each do |item|
+      new_array.push item if yield(item)
+    end
+    new_array
+  end
+
+  def my_all?
+    bool = true
+    each do |item|
+      bool = false unless yield(item)
+    end
+    bool
+  end
+
+  def my_any?
+    bool = false
+    each do |item|
+      bool = true if yield(item)
+    end
+    bool
+  end
+
+  def my_none?
+    bool = true
+    each do |item|
+      bool = false if yield(item)
+    end
+    bool
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      each do |item|
+        count += 1 if yield(item)
+      end
+    else
+      each do |item|
+        count += 1
+      end
+    end
+    count
+  end
+
+  def my_map
+    new_array = []
+    each do |item|
+      new_array.push yield(item)
+    end
+    new_array
+  end
+
+  def my_inject(initial_value)
+    memo = initial_value
+    each do |item|
+      memo = yield(memo, item)
+    end
+    memo
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +77,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for item in self
+      yield item
+    end
+  end
 end
